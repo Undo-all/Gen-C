c0add71e720626c3714acad8dcf677385de878b9
Undo-all <undoall2@gmail.com>
2015-09-18 19:27:03 -0400

    add convienent types

--- CGen.hs
+++ CGen.hs
@@ -2,3 +2,20 @@ module CGen where
 
 import AST
 
+unwrapIntVariant :: Type -> IntVariant
+unwrapIntVariant (Integer x) = x
+unwrapIntVarient _           = error "not an integer"
+
+char = Integer Char
+short = Integer Short
+int = Integer Int
+long = Integer Long
+longlong = Integer LongLong
+signed = Signed . unwrapIntVariant
+unsigned = Unsigned . unwrapIntVariant
+float = Floating Float
+double = Floating Double
+void = Void
+ptr = Pointer
+array = Array
+
